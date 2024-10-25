440cfc584f75165f375802bde77a88e026519294
guipiri <gui.soliveiras@gmail.com>
2024-10-25 16:19:08 -0300

    fix: remove ',' and change -crypy to -1

--- script.sh
+++ script.sh
@@ -5,8 +5,8 @@ echo "Criando diretórios, grupos, alterando donos e permissões..."
 mkdir /publico
 chmod 777 /publico
 
-FOLDERS = ('/adm', '/ven', '/sec')
-GROUPS = ('GRP_ADM','GRP_VEN','GRP_SEC')
+FOLDERS = ('/adm' '/ven' '/sec')
+GROUPS = ('GRP_ADM' 'GRP_VEN' 'GRP_SEC')
 
 for((cont=0;cont<${#FOLDERS[@]};cont++)); do
     mkdir ${FOLDERS[$cont]}
@@ -18,15 +18,15 @@ done
 echo "Criando usuários e adicionando-os aos devidos grupos..."
 
 for i in carlos maria joao; do
-  useradd $i -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+  useradd $i -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
 done
 
 for i in debora sebastiana roberto; do
-  useradd $i -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+  useradd $i -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
 done
 
 for i in josefina amanda rogerio; do
-  useradd $i -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+  useradd $i -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
 done
 
 echo "Fim....."
